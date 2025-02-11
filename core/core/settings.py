5a8e077727172bdedf9b4b429db36837e566174f
Cristian Moyano <cristianmoyano.mza@gmail.com>
2025-02-11 11:45:15 -0300

    add prod config

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cv0@m0xwnujs6b0n3^$l(g)=n(*v(&jp^a3b6mm%v(d=*r&qem'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
