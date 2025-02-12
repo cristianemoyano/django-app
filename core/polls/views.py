301913f266f7dbd551c28da73930bc29833b640b
Cristian Moyano <cristianmoyano.mza@gmail.com>
2025-02-11 23:33:30 -0300

    add polls

--- core/polls/views.py
+++ core/polls/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {"latest_question_list": [] }
+    context = {"latest_question_list": [
+        {"id": 1, "question_text": f"What's new?", "pub_date": "2021-08-01"},
+        {"id": 2, "question_text": "What's up?", "pub_date": "2021-08-02"},
+        {"id": 3, "question_text": "What's next?", "pub_date": "2021-08-03"},
+    ]}
     return render(request, "polls/index.html", context)
